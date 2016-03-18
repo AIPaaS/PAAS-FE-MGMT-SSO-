ea4f8a99359c775d2e05cc5bda542d4fee4c03d2
mapl-pisces <mapl@asiainfo.com>
2016-03-18 15:06:32 +0800

    添加接口vertifymoduId

--- src/main/java/com/aic/paas/csso/mvc/ExternalOperationMvc.java
+++ src/main/java/com/aic/paas/csso/mvc/ExternalOperationMvc.java
@@ -92,7 +92,7 @@ public class ExternalOperationMvc {
 	 * @return
 	 */
 	@RequestMapping("/verifyModuCode")
-	public @ResponseBody String verifyModuId(HttpServletRequest request, HttpServletResponse response,
+	public @ResponseBody String verifyModuCode(HttpServletRequest request, HttpServletResponse response,
 			 Long opId, String moduCode) {	
 		
 		boolean verifyResult=  SysFrameUtil.verifyModuCode(opId, moduCode);	
@@ -100,6 +100,21 @@ public class ExternalOperationMvc {
 		return String.valueOf(verifyResult);
 	}
 	
+	
+	/**
+	 * 验证用户是否具有指定模块的权限
+	 * @param opId : 用户ID
+	 * @param moduId : 被验证的模块Id
+	 * @return
+	 */
+	@RequestMapping("/verifyModuId")
+	public @ResponseBody String verifyModuId(HttpServletRequest request, HttpServletResponse response,
+			 Long opId, Long moduId) {	
+		
+		boolean verifyResult=  SysFrameUtil.verifyModuId(opId, moduId);	
+		
+		return String.valueOf(verifyResult);
+	}
 	
 	
 }
